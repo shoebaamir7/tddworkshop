d141263247677e6d7943d6519748db9544972bf0
unknown <shoebaamir7@gmail.com>
2017-09-18 19:36:04 +0530

    Calculator

---
+++ tests/phpreboot/tddworkshop/CalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace phpreboot\tddworkshop;
+
+use phpreboot\tddworkshop\Calculator;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    private $calculator;
+
+    public function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+
+    public function tearDown()
+    {
+        $this->calculator = null;
+    }
+
+    public function testAddReturnsAnInteger()
+    {
+        $result = $this->calculator->add();
+
+        $this->assertInternalType('integer', $result, 'Result of `add` is not an integer.');
+    }
+}
